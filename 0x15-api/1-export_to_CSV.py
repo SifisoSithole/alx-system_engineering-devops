1dd0fbb41508e5d81d6acb5bf539f70a7a39cc2b
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-16 18:45:19 +0200

    add: 0-gather_data_from_an_API.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,20 +5,20 @@ Script that returns information from an api
 
 if __name__ == "__main__":
     import requests
-    import sys
+    from sys import argv as a
 
-    url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
+    url = 'https://jsonplaceholder.typicode.com/users/%s/todos' % a[1]
     req = requests.get(url)
-    tasks = []
+    t = []
     task_st = []
     for task in req.json():
         task_st.append(task.get('completed'))
-        tasks.append(task.get('title'))
-    url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+        t.append(task.get('title'))
+    url = 'https://jsonplaceholder.typicode.com/users/%s' % a[1]
     req = requests.get(url)
-    name = req.json().get('username')
+    n = req.json().get('username')
 
-    with open(f'{sys.argv[1]}.csv', 'w') as f:
+    with open('{}.csv'.format(a[1]), 'w') as f:
         for i in range(len(task_st)):
-            m = f'"{sys.argv[1]}","{name}","{task_st[i]}","{tasks[i]}"\n'
+            m = '"%s","%s","%s","%s"\n' % (a[1], n, task_st[i], t[i])
             f.write(m)
