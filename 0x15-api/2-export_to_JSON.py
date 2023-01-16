02362180b210b5ef77853ff84e428c2ab85c79ae
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-16 18:49:08 +0200

    add: 0-gather_data_from_an_API.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,24 +6,25 @@ Script that returns information from an api
 if __name__ == "__main__":
     import json
     import requests
-    import sys
+    from sys import argv as a
 
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com/users/%s/todos' % a[1]
     req = requests.get(url)
     tasks = []
     task_st = []
     for task in req.json():
         task_st.append(task.get('completed'))
         tasks.append(task.get('title'))
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(a[1])
     req = requests.get(url)
     name = req.json().get('username')
 
     dict_list = []
     for i in range(len(task_st)):
         my_dict = {}
-        my_dict.update({"task": tasks[i], "completed": task_st[i], "username": name})
+        my_dict.update({"task": tasks[i], "completed": task_st[i],
+                        "username": name})
         dict_list.append(my_dict)
 
-    with open("{}.json".format(sys.argv[1]), "w") as f:
-        json.dump({"{}".format(str(sys.argv[1])): dict_list}, f)
+    with open("{}.json".format(a[1]), "w") as f:
+        json.dump({"{}".format(a[1]): dict_list}, f)
