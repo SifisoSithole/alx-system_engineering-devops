5a6b0ccc32f2a9142ff4d17725e1ff38bd792c1b
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-31 13:11:41 +0200

    add: 100-count.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,15 +6,9 @@ after = None
 
 
 def recurse(subreddit, hot_list=[]):
-    """     Args:
-        subreddit: subreddit name
-        hot_list: list of hot titles in subreddit
-        after: last hot_item appended to hot_list
-    Returns:
-        a list containing the titles of all hot articles for the subreddit
-        or None if queried subreddit is invalid """
+    """recursive function """
     global after
-    headers = {'User-Agent': 'xica369'}
+    headers = {'User-Agent': 'kia2021'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'after': after}
     response = requests.get(url, headers=headers, allow_redirects=False,
