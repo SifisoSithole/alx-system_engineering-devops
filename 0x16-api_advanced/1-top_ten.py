38e338f4e0212f888cbb68d0605a3cce85653927
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-31 12:21:47 +0200

    add: 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles of the first 10 hot
+posts listed for a given subreddit
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """ top ten posts """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    h = {'User-Agent': 'kia1234'}
+    p = {'limit': 10}
+    r = requests.get(url, headers=h, params=p, allow_redirects=False)
+
+    if r.status_code == 200:
+        titles = r.json().get('data').get('children')
+        for title in titles:
+            print(title.get('data').get('title'))
+    else:
+        print(None)
